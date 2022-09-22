5799f90ca15cd690df30f8a5d67a69bc49546b37
mariusx5 <marius.ulrich1gmx.net>
2022-09-22 10:48:20 +0200

    added sign-in component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Home from './routes/home/home.component';
 import {Routes, Route, Outlet} from 'react-router-dom';
 import Navigation from './routes/navigation/navigation.component';
 import SignIn from './routes/sign-in/sign-in.component';
+import { setDoc } from 'firebase/firestore';
 
 const Shop = () => {
   return <h1>I am the shop page</h1>
@@ -18,6 +19,7 @@ const App = () => {
       </Route>
     </Routes>
   );
+
 };
 
 export default App;
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -47,9 +47,29 @@ export const db = getFirestore();
 export const createUserDocumentFromAuth = async (userAuth) =>{
     const userDocRef = doc(db, 'users', userAuth.uid);
 
-    console.log(userDocRef);
+    //console.log(userDocRef);
 
     const userSnapShot = await getDoc(userDocRef);
-    console.log(userSnapShot)
-    console.log(userSnapShot.exists());
+/*     console.log(userSnapShot)
+    console.log(userSnapShot.exists()); */
+
+
+//set user data if not existing
+  if(!userSnapShot.exists()){
+    const { displayName, email} = userAuth;
+    const createdAt = new Date();
+    
+    try {
+      await setDoc(userDocRef,{
+        displayName,
+        email,
+        createdAt
+      });
+    } catch(error){
+        console.log('error creating the user', error.message);
+    }
+
+    return userDocRef;
+  }
+
 }
